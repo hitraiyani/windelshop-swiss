e6c4f59c0d0db5df72eae1f8b58dc69f18033541
Ravipatelbg <114903445+Ravipatelbg@users.noreply.github.com>
2023-07-27 14:59:18 +0530

    Push the code for link.

--- app/components/CtaBanner.jsx
+++ app/components/CtaBanner.jsx
@@ -1,16 +1,17 @@
 import {flattenConnection, Image, Money, useMoney} from '@shopify/hydrogen';
 
-
 export function CtaBanner({banner}) {
   return (
     <section className={`cta-banner-section py-[15px] md:py-[30px]`}>
       <div className="container">
         <div className="img-wrap">
-          <Image 
-             className="max-w-full w-full h-auto"
-             data={banner?.reference?.image}
-             alt={banner?.reference?.alt}
-          />
+          <a  className="max-w-full w-full h-auto block" href="/collections/windeln-feuchttucher">
+            <Image
+              className="max-w-full w-full h-auto"
+              data={banner?.reference?.image}
+              alt={banner?.reference?.alt}
+            />
+          </a>
         </div>
       </div>
     </section>
